2282e74fccc584561e40f53a48354a8774abdb91
Morari Cosmin <cosminmorari99@yahoo.com>
2024-12-08 21:18:58 +0200

    modificare link catre conectare mail

---
+++ .history/resources/views/users/same_hotel.blade_20241208211105.php
@@ -0,0 +1,30 @@
+<table border="1" style="width: 100%; border-collapse: collapse;">
+    <thead>
+        <tr>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Role</th>
+            <th>Department</th>
+            <th>Hotel</th>
+            <th>Manager Name</th>
+            <th>Company Name</th>
+            <th>County</th>
+            <th>Company Address</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->status ? 'Active' : 'Inactive' }}</td>
+                <td>{{ $user->role }}</td>
+                <td>{{ $user->department ? $user->department->name : 'N/A' }}</td>
+                <td>{{ $user->hotel ? $user->hotel->name : 'N/A' }}</td>
+                <td>{{ $user->manager_name ?? 'N/A' }}</td>
+                <td>{{ $user->company_name ?? 'N/A' }}</td>
+                <td>{{ $user->county ?? 'N/A' }}</td>
+                <td>{{ $user->company_address ?? 'N/A' }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
