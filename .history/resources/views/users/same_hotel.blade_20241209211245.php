1bcbcca5d670389494fee9ad99af5c02c5e56dca
Morari Cosmin <cosminmorari99@yahoo.com>
2024-12-09 21:13:56 +0200

    modificare link catre conectare mail

--- .history/resources/views/users/same_hotel.blade_20241209211245.php
+++ .history/resources/views/users/same_hotel.blade_20241209211245.php
@@ -96,6 +96,23 @@
 
     <!-- Hero Section -->
     <section id="hero" class="hero section accent-background">
+
+      <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+        <div class="row gy-5 justify-content-between">
+          <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+            <h2><span>Welcome to </span><span class="accent">Impact</span></h2>
+            <p>Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
+            <div class="d-flex">
+              <a href="#about" class="btn-get-started">Get Started</a>
+              <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+            </div>
+          </div>
+          <div class="col-lg-5 order-1 order-lg-2">
+            <img src="{{ asset('images/hero-img.svg') }}" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+
       <div class="icon-boxes position-relative" data-aos="fade-up" data-aos-delay="200">
         <div class="container position-relative">
           <div class="row gy-4 mt-5">
